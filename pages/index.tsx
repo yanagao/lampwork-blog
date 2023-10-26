1e6b0b3162860bedf63821f7db75867a726e1b57
ya gao <gaoya@inftech.ai>
2023-10-26 14:34:48 +0800

    feat: 去掉alert

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,7 +19,7 @@ export default function Index({ allPosts }: Props) {
     <>
       <Layout>
         <Head>
-          <title>{`Next.js Blog Example with ${CMS_NAME}`}</title>
+          <title>Yana' lampwork blog</title>
         </Head>
         <Container>
           <Intro />
